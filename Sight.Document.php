a0a66b1eb873a5cd3c3ad42a8b80db9ab9d8cbc6
David Millet <david.millet@gmail.com>
2012-09-19 13:32:42 -0700

    declaring methods as public static that should have been public static

---
+++ Sight.Document.php
@@ -0,0 +1,21 @@
+<?
+
+namespace Sight;
+
+class Document {
+	private $path;
+	
+	function __construct($path) {
+		$this->path = $path;
+	}
+
+	function send() {
+		
+		$finfo = finfo_open(FILEINFO_MIME_TYPE);
+		$mimetype = finfo_file($finfo, $path);
+
+		header('Content-type: ' . $mimetype);
+		//header('Content-Disposition: attachment; filename='.basename($this->path));
+		readfile($this->path);
+	}
+}
