fb8a8ced7b596326fc39f5b3c6e3cc915a471279
David Millet <david.millet@gmail.com>
2013-08-02 08:45:36 -0700

    moving docs/includes/ dir to be just includes/ ... renaming docs/ dir to pages/

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require_once("Sight.php");
 
 $sight = new Sight("Sight!");
 
-$sight->routeError(404,"docs/404.html");
+$sight->routeError(404,"pages/404.html");
 
-$sight->setDefaultTemplate("docs/includes/template.html");
+$sight->setDefaultTemplate("includes/template.html");
 
 $sight->route(
 	"/^.*$/",
-	"docs/$0.html"
+	"pages/$0.html"
 );
 
 $sight->respond();
